37d42e6d8ad7f7b39375671d0bc812e39e638ecf
kaushal <kolhekaushal2003@gmail.com>
2024-10-21 02:36:45 +0530

    added service-registry server

---
+++ Service-registry/Service-registry/src/test/java/com/servers/Service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servers.Service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
